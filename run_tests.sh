79c6001bbcf92996e1f812d41bbfeb87e62b28a7
Nicholas Saparoff <nick.saparoff@gmail.com>
2019-11-19 19:04:07 -0500

    updated run_tests.sh script
    only try to install phenome-core or get phenome-extensions if they do not exist...

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-# must have the PHENOME core installed
-pip install phenome-core --extra-index-url https://api.packagr.app/AvVcpYfO2/
+has_core=$(pip list modules | grep phenome-core)
 
-# must have the PHENOME extensions installed (for tests AND packaging)
-git clone https://${GH_TOKEN}@github.com/holitics/phenome-extensions phenome
+if [ -z "$has_core" ]; then
+	echo "Installing Phenome Core..."
+	pip install phenome-core --extra-index-url https://api.packagr.app/AvVcpYfO2/
+fi
+
+if [ ! -d "phenome" ]; then
+	echo "Installing Phenome Extensions..."
+	# must have the PHENOME extensions installed
+	# the packaged distro has the needed extensions, but when
+	# cloning directly from GitHub, the dependencies may not be there.
+	git clone https://github.com/holitics/phenome-extensions phenome
+fi
 
 # run the tests
 pytest
